c17af4a6e628567ac377d554e2dc283eb54c4732
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2022-10-01 17:08:36 -0500

    add Parrot class

--- src/main/java/no/data/Main.java
+++ src/main/java/no/data/Main.java
@@ -2,6 +2,7 @@ package no.data;
 
 public class Main {
     public static void main(String[] args) {
+        Parrot parrot = new Parrot();
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/no/data/Parrot.java
@@ -0,0 +1,15 @@
+package no.data;
+
+public class Parrot {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
