60938029bd2ad349e0fca96392e050a596e8e14e
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2022-10-04 21:53:42 -0500

    use configuration class
    Changes committed:
    	modified:   src/main/java/no/data/Main.java

--- src/main/java/no/data/Main.java
+++ src/main/java/no/data/Main.java
@@ -1,12 +1,10 @@
 package no.data;
 
+import no.data.configuration.ProjectConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-
-        Parrot parrot = new Parrot();
-        System.out.println("Hello world!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
     }
 }
