c623f9a9da4c8de3c9f96e377b3d24c6252f3eb2
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2022-10-04 21:42:00 -0500

    add Spring Context and create Parrot object
    Changes committed:
    	modified:   src/main/java/no/data/Main.java

--- src/main/java/no/data/Main.java
+++ src/main/java/no/data/Main.java
@@ -1,7 +1,11 @@
 package no.data;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
         Parrot parrot = new Parrot();
         System.out.println("Hello world!");
     }
