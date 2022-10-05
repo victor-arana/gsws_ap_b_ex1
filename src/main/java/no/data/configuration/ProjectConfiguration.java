6f5671ba818cc2571e613934323cb5c41cb522c8
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2022-10-04 21:50:50 -0500

    add Parrot to the Spring Context
    Changes committed:
    	modified:   src/main/java/no/data/configuration/ProjectConfiguration.java

--- src/main/java/no/data/configuration/ProjectConfiguration.java
+++ src/main/java/no/data/configuration/ProjectConfiguration.java
@@ -1,8 +1,17 @@
 package no.data.configuration;
 
+import no.data.Parrot;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProjectConfiguration {
+
+    @Bean
+    Parrot parrot(){
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
 
 }
